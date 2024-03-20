9c9f4fc843f1c8de1bcdb798f3e91be107c77d0f
Henri <henri.breuer@outlook.de>
2024-03-20 15:51:47 +0100

    added flutter packages and folder setup

---
+++ frontend/lib/src/constants/app_spacing.dart
@@ -0,0 +1,150 @@
+// all the different sizes used
+
+import 'package:flutter/Widgets.dart';
+
+class Spacing {
+  static const p8 = 8.0;
+  static const p16 = 16.0;
+  static const p24 = 24.0;
+  static const p32 = 32.0;
+  static const p40 = 40.0;
+  static const p48 = 48.0;
+  static const p56 = 56.0;
+  static const p64 = 64.0;
+  static const p72 = 72.0;
+  static const p80 = 80.0;
+  static const p88 = 88.0;
+  static const p96 = 96.0;
+  static const p104 = 104.0;
+  static const p112 = 112.0;
+  static const p120 = 120.0;
+  static const p128 = 128.0;
+  static const p136 = 136.0;
+  static const p144 = 144.0;
+  static const p152 = 152.0;
+  static const p160 = 160.0;
+}
+
+// Constant width spacing
+const gapW8 = SizedBox(
+  width: Spacing.p8,
+);
+const gapW16 = SizedBox(
+  width: Spacing.p16,
+);
+const gapW24 = SizedBox(
+  width: Spacing.p24,
+);
+const gapW32 = SizedBox(
+  width: Spacing.p32,
+);
+const gapW40 = SizedBox(
+  width: Spacing.p40,
+);
+const gapW48 = SizedBox(
+  width: Spacing.p48,
+);
+const gapW56 = SizedBox(
+  width: Spacing.p56,
+);
+const gapW64 = SizedBox(
+  width: Spacing.p64,
+);
+const gapW72 = SizedBox(
+  width: Spacing.p72,
+);
+const gapW80 = SizedBox(
+  width: Spacing.p80,
+);
+const gapW88 = SizedBox(
+  width: Spacing.p88,
+);
+const gapW96 = SizedBox(
+  width: Spacing.p96,
+);
+const gapW104 = SizedBox(
+  width: Spacing.p104,
+);
+const gapW112 = SizedBox(
+  width: Spacing.p112,
+);
+const gapW120 = SizedBox(
+  width: Spacing.p120,
+);
+const gapW128 = SizedBox(
+  width: Spacing.p128,
+);
+const gapW136 = SizedBox(
+  width: Spacing.p136,
+);
+const gapW144 = SizedBox(
+  width: Spacing.p144,
+);
+const gapW152 = SizedBox(
+  width: Spacing.p152,
+);
+const gapW160 = SizedBox(
+  width: Spacing.p160,
+);
+
+// Constant height spacing
+const gapH8 = SizedBox(
+  height: Spacing.p8,
+);
+const gapH16 = SizedBox(
+  height: Spacing.p16,
+);
+const gapH24 = SizedBox(
+  height: Spacing.p24,
+);
+const gapH32 = SizedBox(
+  height: Spacing.p32,
+);
+const gapH40 = SizedBox(
+  height: Spacing.p40,
+);
+const gapH48 = SizedBox(
+  height: Spacing.p48,
+);
+const gapH56 = SizedBox(
+  height: Spacing.p56,
+);
+const gapH64 = SizedBox(
+  height: Spacing.p64,
+);
+const gapH72 = SizedBox(
+  height: Spacing.p72,
+);
+const gapH80 = SizedBox(
+  height: Spacing.p80,
+);
+const gapH88 = SizedBox(
+  height: Spacing.p88,
+);
+const gapH96 = SizedBox(
+  height: Spacing.p96,
+);
+const gapH104 = SizedBox(
+  height: Spacing.p104,
+);
+const gapH112 = SizedBox(
+  height: Spacing.p112,
+);
+const gapH120 = SizedBox(
+  height: Spacing.p120,
+);
+const gapH128 = SizedBox(
+  height: Spacing.p128,
+);
+const gapH136 = SizedBox(
+  height: Spacing.p136,
+);
+const gapH144 = SizedBox(
+  height: Spacing.p144,
+);
+const gapH152 = SizedBox(
+  height: Spacing.p152,
+);
+const gapH160 = SizedBox(
+  height: Spacing.p160,
+);
